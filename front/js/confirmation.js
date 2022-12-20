f06d0b7fd50e75c39e74a8266dbf7d22215d99f6
Romain <chaventre.romain@orange.fr>
2022-12-20 09:17:07 +0100

    verification product et debut cart

--- front/js/confirmation.js
+++
@@ -1,8 +0,0 @@
-//Récupération du numéro de commande dans l'URL
-var str = window.location.href;
-var url = new URL(str);
-var idOrderURL = url.searchParams.get("orderId");
-
-//Affichage du numéro de commande
-var orderIdNumberElt = document.querySelector('#orderId');
-orderIdNumberElt.innerHTML = idOrderURL;
